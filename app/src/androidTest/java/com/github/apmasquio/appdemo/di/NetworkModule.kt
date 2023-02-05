98f0d10c8f8f85a3dd902b21329c0494866a75d3
alexandre.porto <alexandre.porto@wansoft.com.br>
2023-02-04 22:25:48 -0300

    feat: data and di packages

---
+++ app/src/androidTest/java/com/github/apmasquio/appdemo/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.github.apmasquio.appdemo.di
+
+
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+}
